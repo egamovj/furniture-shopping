93af1a6d54f0f642e530ce80968d3a6eb655e0de
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-01-22 22:42:32 +0500

    rendered datas

--- cart.js
+++ cart.js
@@ -1,7 +1,10 @@
+import products from "./products";
+
 const cart = () => {
     let iconCart = document.querySelector('.icon-cart');
     let closeBtn = document.querySelector('.cartTab .close');
     let body = document.querySelector('body');
+    let cart = [];
 
     iconCart.addEventListener('click', () => {
         body.classList.toggle('activeTabCart');
--- index.js
+++ index.js
@@ -18,5 +18,18 @@ const loadTemplate = () => {
 loadTemplate();
 
 const initApp = () => {
-
+    let listProduct = document.querySelector('.listProduct');
+    listProduct.innerHTML = null;
+    products.forEach(product => {
+        let newProduct = document.createElement('div')
+        newProduct.classList.add('item');
+        newProduct.innerHTML = 
+        `
+            <img src='${product.image}' />
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>
+            <button class="addCart" data-id="${product.id}">Add To Cart</button>
+        `;
+        listProduct.appendChild(newProduct)
+    })
 }
